33aa6291b718cae838fa66f521346fb40f1cd077
beltramoe <beltramoe@gmail.com>
2023-11-26 09:09:15 +0000

    Update

--- createenv.sh
+++ createenv.sh
@@ -1,13 +1,23 @@
 eval "$(conda shell.bash hook)"
 conda remove -n econ -y --all
-conda create -n econ -y python=3.10
+conda create -n econ -y python=3.8
 conda activate econ
 
-conda install pytorch==1.13.0 torchvision==0.14.0 torchaudio==0.13.0 pytorch-cuda=11.6 -c pytorch -c nvidia -y
+conda install pytorch==1.13.1 torchvision==0.14.1 torchaudio==0.13.1 pytorch-cuda=11.6 -c pytorch -c nvidia -y
+conda install -c anaconda cython pip -y
 conda install -c bottler nvidiacub -y
 conda install -c conda-forge fvcore iopath pyembree -y
-conda install -c anaconda cupy cython pip -y
+conda install -c conda-forge pytorch-lightning kornia -y
 
-pip install -r requirements.txt
+pip install cupy-cuda116
+pip install --no-index --no-cache-dir pytorch3d -f https://dl.fbaipublicfiles.com/pytorch3d/packaging/wheels/py38_cu116_pyt1130/download.html
+
+pip install -r requirements2.txt
+
+cd lib/common/libmesh
+python setup.py build_ext --inplace
+cd ../libvoxelize
+python setup.py build_ext --inplace
+cd ../../../
 
 conda deactivate
